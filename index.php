78134e2f101f2b341838716c643a20a89ee54188
JuanR-T <juan.m13@hotmail;fr>
2022-02-12 19:23:36 +0100

    sorting + pagination

--- index.php
+++ index.php
@@ -17,36 +17,59 @@ $query->execute();
 $resultMovies = $query->fetch();
 $nbMovies = (int) $resultMovies['nb_film'];
 
-//Combien de pages me faut-il ?
+// Combien de locations par films ?
+
+$sqlTotalRentals = "SELECT COUNT(inventory.inventory_id) 
+FROM rental 
+LEFT JOIN inventory 
+ON rental.inventory_id = inventory.inventory_id 
+WHERE film.film_id = inventory.film_id) as rentals";
+
+$query = $databaseConnection->prepare($sqlTotalRentals);
+$query->execute();
+$resultRentals = $query->fetch();
+$nbRentals = (int) $resultRentals['nb_rentals'];
+var_dump($nbRentals);
+
+//Sorting
+
+$getSorting = "";
+$order = "";
+if (isset($_GET['categories']) && !empty($_GET['categories'])) {
+    $getSorting = $_GET['categories'];
+    $order = "ORDER BY" . " " . $_GET['categories'];
+}
 
 $moviesPerPage = 10;
+if (isset($_GET['moviesPerPage']) && !empty($_GET['moviesPerPage'])) {
+    $moviesPerPage = $_GET['moviesPerPage'];
+}
+
+//Combien de pages me faut-il ?
+
 $pagesNumber = ceil($nbMovies / $moviesPerPage);
+$pagesMax = $pagesNumber;
 
 //Premier film de la liste
+
 $firstMovie = ($currentPage * $moviesPerPage) - $moviesPerPage;
-
-//Pagination
-
-$sqlPages = "SELECT title, category, rating, FID,price FROM film_list LIMIT :firstMovie, :moviesPerPage";
+$sqlPages = "SELECT film.film_id, title, rental_rate, rating, category.name, 
+COUNT(rental.rental_id) as nb_rentals FROM film 
+LEFT JOIN film_category ON film.film_id = film_category.film_id
+LEFT JOIN category ON film_category.category_id = category.category_id
+LEFT JOIN inventory ON film.film_id = inventory.film_id
+LEFT JOIN rental ON rental.inventory_id = inventory.inventory_id
+GROUP BY film.film_id $order
+LIMIT :firstMovie, :moviesPerPage";
 
 $result = $databaseConnection->prepare($sqlPages);
-
 $result->bindValue(':firstMovie', $firstMovie, PDO::PARAM_INT);
 $result->bindValue(':moviesPerPage', $moviesPerPage, PDO::PARAM_INT);
 $result->execute();
 
 // On récupère les valeurs dans un tableau associatif
+
 $movies = $result->fetchAll(PDO::FETCH_ASSOC);
-
-//Tri des films
-
-$columns = array('film', 'genre', 'rental', 'rating');
-$column = isset($_GET['column']) && in_array($_GET['column'], $columns) ? $_GET['column'] : $columns[0];
-$sort_order = isset($_GET['order']) && strtolower($_GET['order']) == 'desc' ? 'DESC' : 'ASC';
-$up_or_down = str_replace(array('ASC', 'DESC'), array('up', 'down'), $sort_order);
-$asc_or_desc = $sort_order == 'ASC' ? 'desc' : 'asc';
-$add_class = ' class="highlight"';
-$total_reg = "10";
 
 //Display table
 
@@ -60,6 +83,7 @@ $total_reg = "10";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SGBDR</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
         html {
             font-family: Tahoma, Geneva, sans-serif;
@@ -111,21 +135,65 @@ $total_reg = "10";
 </head>
 
 <body>
-    <table>
+    <div class="text-center d-flex flex-column align-items-center">
+        <form class="text-center d-inline-flex p-2 bd-highlight" method="GET">
+            <select class="browser-default custom-select mx-2" name="moviesPerPage">
+                <option value="" disabled selected>Afficher <?= $moviesPerPage ?> films</option>
+                <option value="10">Afficher 10 films</option>
+                <option value="20">Afficher 20 films</option>
+                <option value="30">Afficher 30 films</option>
+                <option value="40">Afficher 40 films</option>
+                <option value="50">Afficher 50 films</option>
+                <option value="100">Afficher 100 films</option>
+            </select>
+            <select class="browser-default custom-select mx-2" name="categories">
+                <option value="">Trié par :</option>
+                <option value="title ASC">Titre ascendant</option>
+                <option value="title DESC">Titre descendant</option>
+                <option value="name ASC">Categorie ascendante</option>
+                <option value="name DESC">Categorie descendante</option>
+                <option value="rating ASC">Notation ascendante</option>
+                <option value="rating DESC">Notation descendante</option>
+                <option value="nb_rentals ASC">Locations ascendante</option>
+                <option value="nb_rentals DESC">Locations descendante</option>
+                <option value="rental_rate ASC">Prix ascendant</option>
+                <option value="rental_rate DESC">Prix descendant</option>
+            </select>
+            <input type="submit" name="submit" value="Get sorting" class="btn btn-primary mx-2 "></input>
+        </form>
+        <nav>
+            <ul class="pagination">
+                <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                    <a href="./?page=<?= $currentPage - 1 ?>&moviesPerPage=<?= urlencode($moviesPerPage) ?>&categories=<?= urlencode($getSorting) ?>" class="page-link">Previous</a>
+                </li>
+
+                <?php for ($pagesNumber = 1; $pagesNumber <= $pagesMax; $pagesNumber++) : ?><?php endfor ?>
+                <li class="page-item <?= ($currentPage == $pagesNumber) ? "active" : "" ?>d-flex align-items-center border border-5 border-primary mx-2 rounded">
+                    <div class="mx-2"><?= $currentPage ?>/<?= $pagesMax ?></div>
+                </li>
+
+                <li class="page-item <?= ($currentPage == $pagesMax) ? "disabled" : "" ?>">
+                    <a href="./?page=<?= $currentPage + 1 ?>&moviesPerPage=<?= urlencode($moviesPerPage) ?>&categories=<?= urlencode($getSorting) ?>" class="page-link">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <table class="table table-dark">
         <tr>
-            <th><a href="<?= $asc_or_desc; ?>">Film<i class="fas fa-sort<?= $column == 'name' ? '-' . $up_or_down : ''; ?>"></i></a></th>
-            <th><a href="<?= $asc_or_desc; ?>">Genre<i class="fas fa-sort<?= $column == 'age' ? '-' . $up_or_down : ''; ?>"></i></a></th>
-            <th><a href="<?= $asc_or_desc; ?>">Rating<i class="fas fa-sort<?= $column == 'joined' ? '-' . $up_or_down : ''; ?>"></i></a></th>
-            <th><a href="<?= $asc_or_desc; ?>">FID<i class="fas fa-sort<?= $column == 'joined' ? '-' . $up_or_down : ''; ?>"></i></a></th>
-            <th><a href="<?= $asc_or_desc; ?>">Rental Price<i class="fas fa-sort<?= $column == 'joined' ? '-' . $up_or_down : ''; ?>"></i></a></th>
+            <th>Film</th>
+            <th>Genre</th>
+            <th>Notation</th>
+            <th>Locations</th>
+            <th>Prix</th>
         </tr>
         <?php foreach ($movies as $movie) { ?>
             <tr>
                 <td><?= $movie['title']; ?></td>
-                <td><?= $movie['category']; ?></td>
+                <td><?= $movie['name']; ?></td>
                 <td><?= $movie['rating']; ?></td>
-                <td><?= $movie['FID']; ?></td>
-                <td><?= $movie['price']; ?></td>
+                <td><?= $movie['nb_rentals'] ?></td>
+                <td><?= $movie['rental_rate']; ?></td>
+
             </tr>
         <?php
         } ?>
